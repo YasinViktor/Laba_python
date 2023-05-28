8bd53d2ec687d29f5bdb2ce299bdf3acbfab99d8
ViktorYasin <victor.yasin05@gmail.com>
2023-05-28 17:20:51 +0300

    add third lab

--- models/abstract_laptop.py
+++ models/abstract_laptop.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class AbstractLaptop(ABC):
     def __init__(self, model, screen_size, ram, storage, battery_life, battery_level):
         self.model = model
@@ -8,7 +9,16 @@ class AbstractLaptop(ABC):
         self.storage = storage
         self.battery_life = battery_life
         self.battery_level = battery_level
+        self.favorite_food_set = set()
 
     @abstractmethod
     def replace_battery(self, capacity_in_hours):
         pass
+
+    def get_attributes_by_type(self, data_type):
+        return {attr: value for attr, value in self.__dict__.items() if isinstance(value, data_type)}
+
+    def __str__(self):
+        return f"--- Abstract Laptop ---\nModel: {self.model}\nScreen Size: {self.screen_size}\n" \
+               f"RAM: {self.ram} GB\nStorage: {self.storage} GB\nBattery Life: {self.battery_life} hours\n" \
+               f"Battery Level: {self.battery_level}%"
